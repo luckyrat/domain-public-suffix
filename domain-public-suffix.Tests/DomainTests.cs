396496dd1f2d74ab33872c8fbe5f49dd9fdb9b02
luckyrat <luckyrat@musites.com>
2018-04-18 22:06:23 +0100

    Better test method name

--- domain-public-suffix.Tests/DomainTests.cs
+++ domain-public-suffix.Tests/DomainTests.cs
@@ -184,17 +184,17 @@ namespace DomainPublicSuffix.Tests
         [TestMethod]
         public void StandardTest1()
         {
-            checkPublicSuffix("COM", null);
+            checkRegistrableDomain("COM", null);
         }
         [TestMethod]
         public void StandardTest2()
         {
-            checkPublicSuffix("example.COM", "example.com");
+            checkRegistrableDomain("example.COM", "example.com");
         }
         [TestMethod]
         public void StandardTest3()
         {
-            checkPublicSuffix("WwW.example.COM", "example.com");
+            checkRegistrableDomain("WwW.example.COM", "example.com");
         }
 
 
@@ -202,113 +202,113 @@ namespace DomainPublicSuffix.Tests
         [TestMethod]
         public void StandardTest4()
         {
-            checkPublicSuffix(".com", null);
+            checkRegistrableDomain(".com", null);
         }
         [TestMethod]
         public void StandardTest5()
         {
-            checkPublicSuffix(".example", null);
+            checkRegistrableDomain(".example", null);
         }
         [TestMethod]
         public void StandardTest6()
         {
-            checkPublicSuffix(".example.com", null);
+            checkRegistrableDomain(".example.com", null);
         }
         [TestMethod]
         public void StandardTest7()
         {
-            checkPublicSuffix(".example.example", null);
+            checkRegistrableDomain(".example.example", null);
         }
 
         // Listed, but non-Internet, TLD.
         [TestMethod]
         public void StandardTest8()
         {
-            checkPublicSuffix("local", null);
+            checkRegistrableDomain("local", null);
         }
         [TestMethod]
         public void StandardTest9()
         {
-            checkPublicSuffix("example.local", null);
+            checkRegistrableDomain("example.local", null);
         }
         [TestMethod]
         public void StandardTest10()
         {
-            checkPublicSuffix("b.example.local", null);
+            checkRegistrableDomain("b.example.local", null);
         }
         [TestMethod]
         public void StandardTest11()
         {
-            checkPublicSuffix("a.b.example.local", null);
+            checkRegistrableDomain("a.b.example.local", null);
         }
         // TLD with only 1 rule.
 
         [TestMethod]
         public void StandardTest12()
         {
-            checkPublicSuffix("biz", null);
+            checkRegistrableDomain("biz", null);
         }
         [TestMethod]
         public void StandardTest13()
         {
-            checkPublicSuffix("domain.biz", "domain.biz");
+            checkRegistrableDomain("domain.biz", "domain.biz");
         }
         [TestMethod]
         public void StandardTest14()
         {
-            checkPublicSuffix("b.domain.biz", "domain.biz");
+            checkRegistrableDomain("b.domain.biz", "domain.biz");
         }
         [TestMethod]
         public void StandardTest15()
         {
-            checkPublicSuffix("a.b.domain.biz", "domain.biz");
+            checkRegistrableDomain("a.b.domain.biz", "domain.biz");
         }
 
         // TLD with some 2-level rules.
         [TestMethod]
         public void StandardTest16()
         {
-            checkPublicSuffix("com", null);
+            checkRegistrableDomain("com", null);
         }
         [TestMethod]
         public void StandardTest17()
         {
-            checkPublicSuffix("example.com", "example.com");
+            checkRegistrableDomain("example.com", "example.com");
         }
         [TestMethod]
         public void StandardTest18()
         {
-            checkPublicSuffix("b.example.com", "example.com");
+            checkRegistrableDomain("b.example.com", "example.com");
         }
         [TestMethod]
         public void StandardTest19()
         {
-            checkPublicSuffix("a.b.example.com", "example.com");
+            checkRegistrableDomain("a.b.example.com", "example.com");
         }
         [TestMethod]
         public void StandardTest20()
         {
-            checkPublicSuffix("uk.com", null);
+            checkRegistrableDomain("uk.com", null);
         }
         [TestMethod]
         public void StandardTest21()
         {
-            checkPublicSuffix("example.uk.com", "example.uk.com");
+            checkRegistrableDomain("example.uk.com", "example.uk.com");
         }
         [TestMethod]
         public void StandardTest22()
         {
-            checkPublicSuffix("b.example.uk.com", "example.uk.com");
+            checkRegistrableDomain("b.example.uk.com", "example.uk.com");
         }
         [TestMethod]
         public void StandardTest23()
         {
-            checkPublicSuffix("a.b.example.uk.com", "example.uk.com");
+            checkRegistrableDomain("a.b.example.uk.com", "example.uk.com");
         }
         [TestMethod]
         public void StandardTest24()
         {
-            checkPublicSuffix("test.ac", "test.ac");
+            checkRegistrableDomain("test.ac", "test.ac");
         }
 
 
@@ -316,22 +316,22 @@ namespace DomainPublicSuffix.Tests
         [TestMethod]
         public void StandardTest25()
         {
-            checkPublicSuffix("mm", null);
+            checkRegistrableDomain("mm", null);
         }
         [TestMethod]
         public void StandardTest26()
         {
-            checkPublicSuffix("c.mm", null);
+            checkRegistrableDomain("c.mm", null);
         }
         [TestMethod]
         public void StandardTest27()
         {
-            checkPublicSuffix("b.c.mm", "b.c.mm");
+            checkRegistrableDomain("b.c.mm", "b.c.mm");
         }
         [TestMethod]
         public void StandardTest28()
         {
-            checkPublicSuffix("a.b.c.mm", "b.c.mm");
+            checkRegistrableDomain("a.b.c.mm", "b.c.mm");
         }
 
 
@@ -339,82 +339,82 @@ namespace DomainPublicSuffix.Tests
         [TestMethod]
         public void StandardTest29()
         {
-            checkPublicSuffix("jp", null);
+            checkRegistrableDomain("jp", null);
         }
         [TestMethod]
         public void StandardTest30()
         {
-            checkPublicSuffix("test.jp", "test.jp");
+            checkRegistrableDomain("test.jp", "test.jp");
         }
         [TestMethod]
         public void StandardTest31()
         {
-            checkPublicSuffix("www.test.jp", "test.jp");
+            checkRegistrableDomain("www.test.jp", "test.jp");
         }
         [TestMethod]
         public void StandardTest32()
         {
-            checkPublicSuffix("ac.jp", null);
+            checkRegistrableDomain("ac.jp", null);
         }
         [TestMethod]
         public void StandardTest33()
         {
-            checkPublicSuffix("test.ac.jp", "test.ac.jp");
+            checkRegistrableDomain("test.ac.jp", "test.ac.jp");
         }
         [TestMethod]
         public void StandardTest34()
         {
-            checkPublicSuffix("www.test.ac.jp", "test.ac.jp");
+            checkRegistrableDomain("www.test.ac.jp", "test.ac.jp");
         }
         [TestMethod]
         public void StandardTest35()
         {
-            checkPublicSuffix("kyoto.jp", null);
+            checkRegistrableDomain("kyoto.jp", null);
         }
         [TestMethod]
         public void StandardTest36()
         {
-            checkPublicSuffix("test.kyoto.jp", "test.kyoto.jp");
+            checkRegistrableDomain("test.kyoto.jp", "test.kyoto.jp");
         }
         [TestMethod]
         public void StandardTest37()
         {
-            checkPublicSuffix("ide.kyoto.jp", null);
+            checkRegistrableDomain("ide.kyoto.jp", null);
         }
         [TestMethod]
         public void StandardTest38()
         {
-            checkPublicSuffix("b.ide.kyoto.jp", "b.ide.kyoto.jp");
+            checkRegistrableDomain("b.ide.kyoto.jp", "b.ide.kyoto.jp");
         }
         [TestMethod]
         public void StandardTest39()
         {
-            checkPublicSuffix("a.b.ide.kyoto.jp", "b.ide.kyoto.jp");
+            checkRegistrableDomain("a.b.ide.kyoto.jp", "b.ide.kyoto.jp");
         }
         [TestMethod]
         public void StandardTest40()
         {
-            checkPublicSuffix("c.kobe.jp", null);
+            checkRegistrableDomain("c.kobe.jp", null);
         }
         [TestMethod]
         public void StandardTest41()
         {
-            checkPublicSuffix("b.c.kobe.jp", "b.c.kobe.jp");
+            checkRegistrableDomain("b.c.kobe.jp", "b.c.kobe.jp");
         }
         [TestMethod]
         public void StandardTest42()
         {
-            checkPublicSuffix("a.b.c.kobe.jp", "b.c.kobe.jp");
+            checkRegistrableDomain("a.b.c.kobe.jp", "b.c.kobe.jp");
         }
         [TestMethod]
         public void StandardTest43()
         {
-            checkPublicSuffix("city.kobe.jp", "city.kobe.jp");
+            checkRegistrableDomain("city.kobe.jp", "city.kobe.jp");
         }
         [TestMethod]
         public void StandardTest44()
         {
-            checkPublicSuffix("www.city.kobe.jp", "city.kobe.jp");
+            checkRegistrableDomain("www.city.kobe.jp", "city.kobe.jp");
         }
 
 
@@ -422,79 +422,79 @@ namespace DomainPublicSuffix.Tests
         [TestMethod]
         public void StandardTest45()
         {
-            checkPublicSuffix("ck", null);
+            checkRegistrableDomain("ck", null);
         }
         [TestMethod]
         public void StandardTest46()
         {
-            checkPublicSuffix("test.ck", null);
+            checkRegistrableDomain("test.ck", null);
         }
         [TestMethod]
         public void StandardTest47()
         {
-            checkPublicSuffix("b.test.ck", "b.test.ck");
+            checkRegistrableDomain("b.test.ck", "b.test.ck");
         }
         [TestMethod]
         public void StandardTest48()
         {
-            checkPublicSuffix("a.b.test.ck", "b.test.ck");
+            checkRegistrableDomain("a.b.test.ck", "b.test.ck");
         }
         [TestMethod]
         public void StandardTest49()
         {
-            checkPublicSuffix("www.ck", "www.ck");
+            checkRegistrableDomain("www.ck", "www.ck");
         }
         [TestMethod]
         public void StandardTest50()
         {
-            checkPublicSuffix("www.www.ck", "www.ck");
+            checkRegistrableDomain("www.www.ck", "www.ck");
         }
 
         // US K12.
         [TestMethod]
         public void StandardTest51()
         {
-            checkPublicSuffix("us", null);
+            checkRegistrableDomain("us", null);
         }
         [TestMethod]
         public void StandardTest52()
         {
-            checkPublicSuffix("test.us", "test.us");
+            checkRegistrableDomain("test.us", "test.us");
         }
         [TestMethod]
         public void StandardTest53()
         {
-            checkPublicSuffix("www.test.us", "test.us");
+            checkRegistrableDomain("www.test.us", "test.us");
         }
         [TestMethod]
         public void StandardTest54()
         {
-            checkPublicSuffix("ak.us", null);
+            checkRegistrableDomain("ak.us", null);
         }
         [TestMethod]
         public void StandardTest55()
         {
-            checkPublicSuffix("test.ak.us", "test.ak.us");
+            checkRegistrableDomain("test.ak.us", "test.ak.us");
         }
         [TestMethod]
         public void StandardTest56()
         {
-            checkPublicSuffix("www.test.ak.us", "test.ak.us");
+            checkRegistrableDomain("www.test.ak.us", "test.ak.us");
         }
         [TestMethod]
         public void StandardTest57()
         {
-            checkPublicSuffix("k12.ak.us", null);
+            checkRegistrableDomain("k12.ak.us", null);
         }
         [TestMethod]
         public void StandardTest58()
         {
-            checkPublicSuffix("test.k12.ak.us", "test.k12.ak.us");
+            checkRegistrableDomain("test.k12.ak.us", "test.k12.ak.us");
         }
         [TestMethod]
         public void StandardTest59()
         {
-            checkPublicSuffix("www.test.k12.ak.us", "test.k12.ak.us");
+            checkRegistrableDomain("www.test.k12.ak.us", "test.k12.ak.us");
         }
 
 
@@ -502,47 +502,47 @@ namespace DomainPublicSuffix.Tests
         [TestMethod]
         public void StandardTest60()
         {
-            checkPublicSuffix("食狮.com.cn", "食狮.com.cn");
+            checkRegistrableDomain("食狮.com.cn", "食狮.com.cn");
         }
         [TestMethod]
         public void StandardTest61()
         {
-            checkPublicSuffix("食狮.公司.cn", "食狮.公司.cn");
+            checkRegistrableDomain("食狮.公司.cn", "食狮.公司.cn");
         }
         [TestMethod]
         public void StandardTest62()
         {
-            checkPublicSuffix("www.食狮.公司.cn", "食狮.公司.cn");
+            checkRegistrableDomain("www.食狮.公司.cn", "食狮.公司.cn");
         }
         [TestMethod]
         public void StandardTest63()
         {
-            checkPublicSuffix("shishi.公司.cn", "shishi.公司.cn");
+            checkRegistrableDomain("shishi.公司.cn", "shishi.公司.cn");
         }
         [TestMethod]
         public void StandardTest64()
         {
-            checkPublicSuffix("公司.cn", null);
+            checkRegistrableDomain("公司.cn", null);
         }
         [TestMethod]
         public void StandardTest65()
         {
-            checkPublicSuffix("食狮.中国", "食狮.中国");
+            checkRegistrableDomain("食狮.中国", "食狮.中国");
         }
         [TestMethod]
         public void StandardTest66()
         {
-            checkPublicSuffix("www.食狮.中国", "食狮.中国");
+            checkRegistrableDomain("www.食狮.中国", "食狮.中国");
         }
         [TestMethod]
         public void StandardTest67()
         {
-            checkPublicSuffix("shishi.中国", "shishi.中国");
+            checkRegistrableDomain("shishi.中国", "shishi.中国");
         }
         [TestMethod]
         public void StandardTest68()
         {
-            checkPublicSuffix("中国", null);
+            checkRegistrableDomain("中国", null);
         }
 
 
@@ -550,47 +550,47 @@ namespace DomainPublicSuffix.Tests
         [TestMethod]
         public void StandardTest69()
         {
-            checkPublicSuffix("xn--85x722f.com.cn", "xn--85x722f.com.cn");
+            checkRegistrableDomain("xn--85x722f.com.cn", "xn--85x722f.com.cn");
         }
         [TestMethod]
         public void StandardTest70()
         {
-            checkPublicSuffix("xn--85x722f.xn--55qx5d.cn", "xn--85x722f.xn--55qx5d.cn");
+            checkRegistrableDomain("xn--85x722f.xn--55qx5d.cn", "xn--85x722f.xn--55qx5d.cn");
         }
         [TestMethod]
         public void StandardTest71()
         {
-            checkPublicSuffix("www.xn--85x722f.xn--55qx5d.cn", "xn--85x722f.xn--55qx5d.cn");
+            checkRegistrableDomain("www.xn--85x722f.xn--55qx5d.cn", "xn--85x722f.xn--55qx5d.cn");
         }
         [TestMethod]
         public void StandardTest72()
         {
-            checkPublicSuffix("shishi.xn--55qx5d.cn", "shishi.xn--55qx5d.cn");
+            checkRegistrableDomain("shishi.xn--55qx5d.cn", "shishi.xn--55qx5d.cn");
         }
         [TestMethod]
         public void StandardTest73()
         {
-            checkPublicSuffix("xn--55qx5d.cn", null);
+            checkRegistrableDomain("xn--55qx5d.cn", null);
         }
         [TestMethod]
         public void StandardTest74()
         {
-            checkPublicSuffix("xn--85x722f.xn--fiqs8s", "xn--85x722f.xn--fiqs8s");
+            checkRegistrableDomain("xn--85x722f.xn--fiqs8s", "xn--85x722f.xn--fiqs8s");
         }
         [TestMethod]
         public void StandardTest75()
         {
-            checkPublicSuffix("www.xn--85x722f.xn--fiqs8s", "xn--85x722f.xn--fiqs8s");
+            checkRegistrableDomain("www.xn--85x722f.xn--fiqs8s", "xn--85x722f.xn--fiqs8s");
         }
         [TestMethod]
         public void StandardTest76()
         {
-            checkPublicSuffix("shishi.xn--fiqs8s", "shishi.xn--fiqs8s");
+            checkRegistrableDomain("shishi.xn--fiqs8s", "shishi.xn--fiqs8s");
         }
         [TestMethod]
         public void StandardTest77()
         {
-            checkPublicSuffix("xn--fiqs8s", null);
+            checkRegistrableDomain("xn--fiqs8s", null);
         }
 
 
@@ -598,25 +598,25 @@ namespace DomainPublicSuffix.Tests
         [TestMethod]
         public void StandardTest78()
         {
-            checkPublicSuffix("example", null);
+            checkRegistrableDomain("example", null);
         }
         [TestMethod]
         public void StandardTest79()
         {
-            checkPublicSuffix("example.example", "example.example");
+            checkRegistrableDomain("example.example", "example.example");
         }
         [TestMethod]
         public void StandardTest80()
         {
-            checkPublicSuffix("b.example.example", "example.example");
+            checkRegistrableDomain("b.example.example", "example.example");
         }
         [TestMethod]
         public void StandardTest81()
         {
-            checkPublicSuffix("a.b.example.example", "example.example");
+            checkRegistrableDomain("a.b.example.example", "example.example");
         }
 
-        private void checkPublicSuffix(string p1, string p2)
+        private void checkRegistrableDomain(string p1, string p2)
         {
             DomainName.TryParse(p1, out outDomain);
             Assert.AreEqual<string>(p2, outDomain != null ? outDomain.RegistrableDomain : null);
