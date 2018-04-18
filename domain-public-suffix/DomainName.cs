dc3d65d2cad862e2d8c785d7fa1f2694e0454b4f
luckyrat <luckyrat@musites.com>
2018-04-18 22:23:59 +0100

    Support unlisted hostnames and Punycode

--- domain-public-suffix/DomainName.cs
+++ domain-public-suffix/DomainName.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Diagnostics;
+using System.Globalization;
 
 namespace DomainPublicSuffix
 {
@@ -51,6 +52,7 @@ namespace DomainPublicSuffix
                 if (!string.IsNullOrEmpty(_domain))
                     output.Append(_domain).Append(".");
                 output.Append(_tld);
+                if (output.Length == 0) return null;
                 return output.ToString();
             }
         }
@@ -68,6 +70,7 @@ namespace DomainPublicSuffix
                 if (!string.IsNullOrEmpty(_domain))
                     output.Append(_domain).Append(".");
                 output.Append(_tld);
+                if (output.Length == 0) return null;
                 return output.ToString();
             }
         }
@@ -126,6 +129,15 @@ namespace DomainPublicSuffix
 
         #endregion
 
+        private static IdnMapping idn = new IdnMapping();
+
+        private static string FormatPartOutput(string part, bool usePunyCode)
+        {
+            if (part == String.Empty) return null;
+            if (usePunyCode) return idn.GetAscii(part);
+            else return part;
+        }
+
         #region Parse domain
 
         /// <summary>
@@ -139,66 +151,105 @@ namespace DomainPublicSuffix
         /// <param name="MatchingRule"></param>
         private static void ParseDomainName(string domainString, out string TLD, out string Domain, out string SubDomain, out TLDRule MatchingRule)
         {
-            TLD = string.Empty;
-            Domain = string.Empty;
-            SubDomain = string.Empty;
+            TLD = null;
+            Domain = null;
+            SubDomain = null;
             MatchingRule = null;
 
             //  If the fqdn is empty, we have a problem already
             if (domainString.Trim() == string.Empty)
                 throw new ArgumentException("The domain cannot be blank");
 
-            //  Next, find the matching rule:
-            MatchingRule = FindMatchingTLDRule(domainString);
+            // We convert incoming strings to Unicode for matching but must
+            // ensure that the returned labels are punycode if that is the 
+            // format we initially received.
+            bool isPunycode = domainString.Contains("xn--");
 
-            //  At this point, no rules match, we have a problem
-            if(MatchingRule == null)
-                throw new FormatException("The domain does not have a recognized TLD");
+            string unicodeString = domainString;
 
-            //  Based on the tld rule found, get the domain (and possibly the subdomain)
+            if (isPunycode)
+            {
+                unicodeString = idn.GetUnicode(domainString);
+            }
+
+            TLDRule fromCache = DomainCache.Get(unicodeString);
+            List<string> allParts;
+            if (fromCache != null)
+            {
+                MatchingRule = fromCache;
+                allParts = new List<string>(unicodeString.Split('.'));
+            } else
+            {
+                // We don't cache leading dot and local matches since they should be fast anyway
+
+                if (unicodeString.StartsWith(".")) return;
+
+                allParts = new List<string>(unicodeString.Split('.'));
+
+                // Special case for local names
+                if (allParts[allParts.Count - 1] == "local") return;
+
+                // Next, find the matching rule:
+                MatchingRule = FindMatchingTLDRule(unicodeString);
+            }
+
+            // At this point, no rules match, must be unlisted
+            if (MatchingRule == null)
+            {
+                if (allParts.Count < 2) return;
+                TLD = FormatPartOutput(allParts[allParts.Count - 1], isPunycode);
+                Domain = FormatPartOutput(allParts[allParts.Count - 2], isPunycode);
+                SubDomain = FormatPartOutput(
+                    string.Join(".", allParts.GetRange(0, allParts.Count - 2).ToArray()),
+                    isPunycode);
+                return;
+            }
+
+            // Based on the tld rule found, get the domain (and possibly the subdomain)
             string tempSudomainAndDomain = string.Empty;
             int tldIndex = 0;
             
-            //  First, determine what type of rule we have, and set the TLD accordingly
+            // First, determine what type of rule we have, and set the TLD accordingly
             switch (MatchingRule.Type)
             {
                 case TLDRule.RuleType.Normal:
-                    tldIndex = domainString.LastIndexOf("." + MatchingRule.Name, StringComparison.InvariantCultureIgnoreCase);
-                    tempSudomainAndDomain = domainString.Substring(0, tldIndex);
-                    TLD = domainString.Substring(tldIndex + 1);
+                    tldIndex = unicodeString.LastIndexOf("." + MatchingRule.Name, StringComparison.InvariantCultureIgnoreCase);
+                    tempSudomainAndDomain = FormatPartOutput(unicodeString.Substring(0, tldIndex), isPunycode);
+                    TLD = FormatPartOutput(unicodeString.Substring(tldIndex + 1), isPunycode);
                     break;
                 case TLDRule.RuleType.Wildcard:
-                    //  This finds the last portion of the TLD...
-                    tldIndex = domainString.LastIndexOf("." + MatchingRule.Name, StringComparison.InvariantCultureIgnoreCase);
-                    tempSudomainAndDomain = domainString.Substring(0, tldIndex);
+                    // This finds the last portion of the TLD...
+                    tldIndex = unicodeString.LastIndexOf("." + MatchingRule.Name, StringComparison.InvariantCultureIgnoreCase);
+                    tempSudomainAndDomain = unicodeString.Substring(0, tldIndex);
 
-                    //  But we need to find the wildcard portion of it:
+                    // But we need to find the wildcard portion of it:
                     tldIndex = tempSudomainAndDomain.LastIndexOf(".", StringComparison.InvariantCultureIgnoreCase);
-                    tempSudomainAndDomain = domainString.Substring(0, tldIndex);
-                    TLD = domainString.Substring(tldIndex + 1);
+                    tempSudomainAndDomain = FormatPartOutput(unicodeString.Substring(0, tldIndex), isPunycode);
+                    TLD = FormatPartOutput(unicodeString.Substring(tldIndex + 1), isPunycode);
                     break;
                 case TLDRule.RuleType.Exception:
-                    tldIndex = domainString.LastIndexOf(".", StringComparison.InvariantCultureIgnoreCase);
-                    tempSudomainAndDomain = domainString.Substring(0, tldIndex);
-                    TLD = domainString.Substring(tldIndex + 1);
+                    var ruleLabelsIndex = MatchingRule.Name.IndexOf(".", StringComparison.InvariantCultureIgnoreCase);
+                    tldIndex = unicodeString.IndexOf(MatchingRule.Name.Substring(ruleLabelsIndex), StringComparison.InvariantCultureIgnoreCase);
+                    tempSudomainAndDomain = FormatPartOutput(unicodeString.Substring(0, tldIndex), isPunycode);
+                    TLD = FormatPartOutput(unicodeString.Substring(tldIndex + 1), isPunycode);
                     break;
             }
 
-            //  See if we have a subdomain:
+            // See if we have a subdomain:
             List<string> lstRemainingParts = new List<string>(tempSudomainAndDomain.Split('.'));
 
-            //  If we have 0 parts left, there is just a tld and no domain or subdomain
-            //  If we have 1 part, it's the domain, and there is no subdomain
-            //  If we have 2+ parts, the last part is the domain, the other parts (combined) are the subdomain
+            // If we have 0 parts left, there is just a tld and no domain or subdomain
+            // If we have 1 part, it's the domain, and there is no subdomain
+            // If we have 2+ parts, the last part is the domain, the other parts (combined) are the subdomain
             if (lstRemainingParts.Count > 0)
             {
-                //  Set the domain:
+                // Set the domain:
                 Domain = lstRemainingParts[lstRemainingParts.Count - 1];
 
-                //  Set the subdomain, if there is one to set:
+                // Set the subdomain, if there is one to set:
                 if (lstRemainingParts.Count > 1)
                 {
-                    //  We strip off the trailing period, too
+                    // We strip off the trailing period, too
                     SubDomain = tempSudomainAndDomain.Substring(0, tempSudomainAndDomain.Length - Domain.Length - 1);
                 }
             }
@@ -252,56 +303,53 @@ namespace DomainPublicSuffix
         /// <returns></returns>
         private static TLDRule FindMatchingTLDRule(string domainString)
         {
-            TLDRule fromCache = DomainCache.Get(domainString);
-            if (fromCache != null)
-                return fromCache;
-
-            if (domainString.StartsWith("."))
-                return null;
-
-            //  Split our domain into parts (based on the '.')
-            //  ...Put these parts in a list
-            //  ...Make sure these parts are in reverse order (we'll be checking rules from the right-most part of the domain)
+            // Split our domain into parts (based on the '.')
+            // ...Put these parts in a list
+            // ...Make sure these parts are in reverse order 
+            // (we'll be checking rules from the right-most part of the domain)
             List<string> lstDomainParts = new List<string>(domainString.Split('.'));
             lstDomainParts.Reverse();
 
-            //  Begin building our partial domain to check rules with:
+            // Begin building our partial domain to check rules with
             string checkAgainst = string.Empty;
 
-            //  Our 'matches' collection:
+            // Our 'matches' collection
             List<TLDRule> ruleMatches = new List<TLDRule>();
 
             foreach (string domainPart in lstDomainParts)
             {
-                //  Add on our next domain part:
+                // Add on our next domain part
                 checkAgainst = string.Format("{0}.{1}", domainPart, checkAgainst);
 
-                //  If we end in a period, strip it off:
+                // If we end in a period, strip it off
                 if (checkAgainst.EndsWith("."))
                     checkAgainst = checkAgainst.Substring(0, checkAgainst.Length - 1);
 
-                //  Try to match an exception rule:
-                foreach (var rule in TLDRulesCache.Instance.TLDRuleList)
+                var ruleTypes = (TLDRule.RuleType[])Enum.GetValues(typeof(TLDRule.RuleType));
+                var ruleList = TLDRulesCache.Instance.TLDRuleLists;
+
+                foreach (var ruleType in ruleTypes)
                 {
-                    if (rule.Name == checkAgainst
-                        && rule.Type == TLDRule.RuleType.Exception)
-                    // As soon as we find any exception rule we have to stop and say that is our best match
+                    // Try to match rule
+                    TLDRule result;
+                    if (ruleList[ruleType].TryGetValue(checkAgainst, out result))
                     {
-                        DomainCache.Set(domainString, rule);
-                        return rule;
+                        // If we find an exception rule, no further processing required
+                        if (ruleType == TLDRule.RuleType.Exception) return result;
+
+                        ruleMatches.Add(result);
                     }
-                }
-                
-                //  Try to match a wildcard or normal rule:
-                foreach (var rule in TLDRulesCache.Instance.TLDRuleList)
-                {
-                    if (rule.Name == checkAgainst
-                        && (rule.Type == TLDRule.RuleType.Wildcard || rule.Type == TLDRule.RuleType.Normal))
-                        ruleMatches.Add(rule);
+                    Debug.WriteLine(string.Format("Domain part {0} matched {1} {2} rules", checkAgainst, result == null ? 0 : 1, ruleType));
                 }
             }
 
-            //  Find the longest match
+            if (ruleMatches.Count == 0)
+            {
+                DomainCache.Set(domainString, null);
+                return null;
+            }
+
+            // Find the longest match
             ruleMatches.Sort(new Comparison<TLDRule>(CompareLength));
             TLDRule primaryMatch = ruleMatches[0];
             DomainCache.Set(domainString, primaryMatch);
